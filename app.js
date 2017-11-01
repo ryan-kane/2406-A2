63cc12378bda72c281d7eeb4ec944747db5ef723
ryan-kane <ryan@thekanes.ca>
2017-11-01 16:37:11 -0400

    finishing touches. fixed 'all the bugs'

--- app.js
+++ app.js
@@ -1,19 +1,31 @@
 var http = require('http');
 var url = require('url');
 var fs = require('fs');
+var win1 = require('./win1');
 
 ROOT_DIR = 'html';
 
-var People = [];
+var sendObj = { 'Player' : null,
+                'Board' : [
+                        ["white", "white", "white"],
+                        ["white", "white", "white"], 
+                        ["white", "white", "white"]],
+                'info' : "",
+                'movingString' : {'visible': false,
+                                'word' : "",
+                                'x' : 100,
+                                'y' : 100,
+                                'xDirection' : 1, //+1 for leftwards, -1 for rightwards
+                                'yDirection' : 1, //+1 for downwards, -1 for upwards
+                                'stringWidth' : 50, //will be updated when drawn
+                                'stringHeight' : 24
+                            }
+            };
+Players = [];
+turn = 0;
 
-var Board = [
-    ["white", "white", "white"],
-    ["white", "white", "white"], 
-    ["white", "white", "white"]];
-    
-var info = "";    
-
-var turn = 0;
+//variable to keep track of canvas size (set to 480x480)
+var canvas = { 'width' : 480, 'height': 480};
 
 var MIME_TYPES = {
     'css': 'text/css',
@@ -43,16 +55,94 @@ var get_mime = function(filename) {
 function win(){
     //logic for if the game is over
     //return who won the game of if its a draw
-    return false;
+    var winner2 = win1(sendObj.Board);
+    if (winner2 != "none"){
+        if(winner2 == "draw"){
+            sendObj.info = "It's a draw!";
+            return true;
+        }else{
+            sendObj.info = winner2 + " wins!";
+            return true;
+        }
+    }
 }
+
+function playerMove(playerObj){
+    sendObj.Player = playerObj.Player;    
+    //if they have a colour
+    if(playerObj.Player != null && playerObj.Player == Players[turn]){
+        
+        posX = playerObj.Move[0]
+        posY = playerObj.Move[1];
+
+        //check space on the Board
+        if(sendObj.Board[posX][posY] == "white"){
+            sendObj.Board[posX][posY] = playerObj.Player;
+            sendObj.info = "good move";
+            if(playerObj.Player == "Orange"){
+                turn = Players.indexOf("Blue");
+            }else{
+                turn = Players.indexOf("Orange");
+            }
+        }else{
+            sendObj.info = "that space is already taken";
+        }
+    }else{
+        if(playerObj.Player != Players[turn]){
+            sendObj.info = "not your turn";
+        }
+        if (playerObj.Player == null) {
+            sendObj.info = "please select a Colour first";
+        }
+    }
+}
+
+function register(playerObj){
+    //for registering
+    //the person already registered can switch if there is only one player
+    if(Players.length < 2){
+        if(Players.includes(playerObj.Player)){
+            sendObj.info = "that colour is taken";
+            sendObj.Player = null;
+        }else{
+            Players.push(playerObj.Player);
+            sendObj.info = null; 
+            sendObj.Player = playerObj.Player;               
+        }
+    }else{
+        sendObj.info = "the game has 2 players";
+        sendObj.Player = null;
+    }
+}
+
+function update(playerObj){
+    sendObj.Player = playerObj.Player;
+    if(Players.length == 0){
+        sendObj.Player = null;
+    }
+    if(Players.length < 2){
+        sendObj.info = "waiting for players to join";
+    }else{
+        sendObj.info = Players[turn] + "'s turn";   
+    }     
+}
+
+function reset(){
+    sendObj.Player = null;
+    sendObj.Board = [["white", "white","white"], ["white", "white","white"], ["white", "white","white"]];
+    sendObj.info = "";
+    Players = [];
+    turn = 0;
+}
+
 
 var server = http.createServer((request, response)=>{
 	
 	var urlObj = url.parse(request.url, true, false);	
-	console.log('\n============================');
-	console.log("PATHNAME: " + urlObj.pathname);
-    console.log("REQUEST: " + urlObj.pathname);
-    console.log("METHOD: " + request.method);
+	// console.log('\n============================');
+	// console.log("PATHNAME: " + urlObj.pathname);
+    // console.log("REQUEST: " + urlObj.pathname);
+    // console.log("METHOD: " + request.method);
 	
     var receivedData = "";
 	
@@ -62,90 +152,54 @@ var server = http.createServer((request, response)=>{
 	});
 	
 	request.on("end", function(){
-		console.log('received data: ', receivedData);
-        console.log('type: ', typeof receivedData);
+		// console.log('received data: ', receivedData);
+        // console.log('type: ', typeof receivedData);
 		
 		//if it is a POST request then echo back the data.
 		if(request.method == "POST"){
-		   var dataObj = JSON.parse(receivedData);
-           console.log('received data object: ', dataObj);
-           console.log('type: ', typeof dataObj);
-           console.log("USER REQUEST: " + dataObj.text);	
-           
-           //what are they trying to do
-           var whosTurn = People[turn % People.length];
-           if(win()){
-               info = "somebody won"
-           }else{
-
-            if(dataObj.text == "Player Move"){
-                //if they have a token
-                if(dataObj.Person.token != null && dataObj.Person.token == whosTurn){
-                    posX = dataObj.Move[0];
-                    posY = dataObj.Move[1];
-                    //check space on the Board
-                    if(Board[posX][posY] == "white"){
-                        Board[posX][posY] = dataObj.Person.token;
-                        info = "good move";
-                        turn++;
-                    }else{
-                        info = "that space is already taken";
-                    }
-                }else{
-                    if(dataObj.Person.token != whosTurn){
-                        info = "not your turn";
-                    }else if (dataObj.Person.token == null) {
-                        info = "please select a Colour first";
-                    }
+           var dataObj = JSON.parse(receivedData);
+        //  console.log('received data object: ', dataObj);
+        //  console.log('type: ', typeof dataObj);
+        //  console.log("USER REQUEST: " + dataObj.text);	
+        //  check if there was a winner
+            if(!win()){
+                 //  what are they trying to do
+                if(dataObj.text == "Player Move"){               
+                    playerMove(dataObj);
+                }else if(dataObj.text == "register"){                   
+                    register(dataObj);
+                }else if(dataObj.text == "update"){
+                    update(dataObj);
                 }
-            }else if(dataObj.text == "register"){
-                //for registering
-                //the person already registered can switch if there is only one player
-                if(People.length < 2){
-                    if(People.includes(dataObj.Person.token)){
-                        info = "that colour is taken";
-                        Person = {token : null};
-                    }else{
-                        People.push(dataObj.Person.token);
-                        info = null;                
-                    }
-                }else{
-                    info = "the game has 2 players";
-                    dataObj.Person = {token : null};
+            }else{
+                if ((sendObj.info == "It's a draw!") ||(sendObj.info == "Blue wins!")||(sendObj.info == "Orange wins!")){
+                    sendObj.movingString.visible = true;
+                    sendObj.movingString.word = sendObj.info;
                 }
-
-            }else if(dataObj.text == "update"){
-                if(People.length != 2){
-                    info = "waiting for players to join";
-                }else{
-                    info = "Player " + People[((turn % People.length)+1)] + "'s turn";    
-                }
-            }else if(dataObj.text == "reset"){
-                for(i = 0; i < 3; i++){
-                    for(j = 0; j < 3; j++){
-                        Board[i][j] = "white";
-                    }
-                }
-                People = [];
-                info = "reset";
+                
+                sendObj.movingString.x = sendObj.movingString.x + 5 * sendObj.movingString.xDirection;
+                sendObj.movingString.y = sendObj.movingString.y + 5 * sendObj.movingString.yDirection;
+    
+                //keep moving word within bounds of canvas
+                if (sendObj.movingString.x + sendObj.movingString.stringWidth > canvas.width) sendObj.movingString.xDirection = -1;
+                if (sendObj.movingString.x < 0) sendObj.movingString.xDirection = 1;
+                if (sendObj.movingString.y > canvas.height) sendObj.movingString.yDirection = -1;
+                if (sendObj.movingString.y - sendObj.movingString.stringHeight < 0) sendObj.movingString.yDirection = 1;
             }
-        }
-           var returnObj = {text : info,
-                            Person : dataObj.Person,
-                            Board : Board};
-            console.log(returnObj);
-		   		   
+            if(dataObj.text == "reset"){
+                reset();
+            }
+            console.log("\n");
+            console.log("Player : " + sendObj.Player);
+            console.log("Board : " + sendObj.Board);
+            console.log("Players : " + Players);
+            console.log(JSON.stringify(sendObj));
+            		   		   
 		   //object to return to client
            response.writeHead(200, {'Content-Type': MIME_TYPES['json']});  
-           response.end(JSON.stringify(returnObj)); //send just the JSON object
+           response.end(JSON.stringify(sendObj)); //send just the JSON object
         }
-        
-
-
-
-
-
-
+    
         if(request.method == "GET"){
 	        //handle GET requests as static file requests
 	        var filePath = ROOT_DIR + urlObj.pathname;
@@ -165,8 +219,6 @@ var server = http.createServer((request, response)=>{
             });
 	    }
 	});
-
-	
 })
 server.listen(3000,()=>{
 	console.log('Server is listening on port 3000');
--- html/appRunner.js
+++ html/appRunner.js
@@ -2,14 +2,10 @@ var canvas = document.getElementById("canvas");
 var context = canvas.getContext("2d");
 
 //objects that will be sent back to the server
-var Person = {token : null};
-var Board = [
-    ["white", "white", "white"],
-    ["white", "white", "white"], 
-    ["white", "white", "white"]];
-
-var pollingTimer;
-
+var sendObj = { 'Player' : null,
+                'text' : "",
+                'Move' : [0,0]
+            };
 /*
 the board will be a an array of arrays like a grid where the 
 arrays act like rows with the index acts like the column
@@ -18,66 +14,62 @@ the board is square so there are the same number of cols as rows
 */
 var size = 3;
 
-function buildBoard(){
-
-    var board = new Array;
-
-    for(i = 0; i < size; i++)
-    {
-        board[i] = new Array;
-        for(j = 0; j < size; j++)
-        {
-            board[i][j] = Board[i][j];
-        }
-    }
-
-
-
-    //iterate through the board and fill the proper squares
+function buildBoard(board, movingString){
+    //iterate through the board array and fill the proper squares on the canvas
     for(i = 0; i < size; i++)
     {
         for(j = 0; j < size; j++)
         {
             context.beginPath();
             context.rect(i * 160, j * 160, 160, 160);
-            context.fillStyle = board[i][j];
+            context.fillStyle = board[i][j]; //this position is a colour
             context.fill();
             context.stroke();
         }
     }
+
+    //create the moving string on the canvas
+    context.fillStyle = "black";
+    context.strokeStyle = "black";
+    //context.font = "30px Arial";
+	movingString.stringWidth = context.measureText(movingString.word).width
+	context.fillText(movingString.word, movingString.x, movingString.y);
+	
 }
 
 /*
 finds which colour is selected and picks registers the user for that colour
 by contacting the server
 
--user has to have selected a colour
+-user has to have selected a colour in order to register
+-user cant already be registered
 */
 function handleRegister(){
     //send the chosen player token
     //check if they are already registered
-    if(Person.token != null){
-        
-    }else{
+    console.log()
+    if(sendObj.Player == null){
         //receive answer and player identity
         if(document.getElementById("radio_Blue").checked){
-            Person.token = "Blue";
-            sendObj = {text : "register", Person : Person};
+            sendObj.Player = "Blue";
+            sendObj.text = "register";
+            document.getElementById("radio_Blue").checked = false;
+            console.log(sendObj);            
             POSTToServer(JSON.stringify(sendObj));
         }else if(document.getElementById("radio_Orange").checked){
-            Person.token = "Orange";
-            sendObj = {text : "register", Person : Person};
+            sendObj.Player = "Orange";
+            sendObj.text = "register";
+            document.getElementById("radio_Orange").checked = false;
+            console.log(sendObj);
             POSTToServer(JSON.stringify(sendObj));
         }else{
-            document.getElementById("warning").innerHTML = "Please select a Colour"
+            document.getElementById("info").innerHTML = "Please select a Colour";
         }
     }
 }
-
 /*
 finds the sqaure that the user clicked based on where the mouse is and
 updates the server
-
 -should not work if the user has not registered
 */
 function handleCanvasClick(event){
@@ -91,60 +83,48 @@ function handleCanvasClick(event){
 
     event.stopPropagation();
     event.preventDefault();
-    
-    console.log("square position : (" + x  + ", " + y + ")");
 
-    //object to send to server
+    sendObj.Move[0] = x;
+    sendObj.Move[1] = y;
+    sendObj.text = "Player Move";
 
-    var sendObj = {Person : Person,
-                    Move : [x, y],
-                    text : "Player Move"};
-
-    document.getElementById("warning").innerHTML = "Playing Move...";
+    document.getElementById("info").innerHTML = "Playing Move...";
     POSTToServer(JSON.stringify(sendObj));
-}
-
-function POSTToServer(sendObj){
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function(){
-        if(this.readyState == 4 && this.status == 200){
-            response = JSON.parse(this.responseText);
-            console.log("response: " + this.responseText);
-            if(response.text) document.getElementById("warning").innerHTML = response.text;
-            if(response.text == "reset"){
-                document.getElementById("warning").innerHTML = "";
-                document.getElementById("who").innerHTML = "";
-            }
-            Person = response.Person;
-            if(Person.token != null){
-                document.getElementById("who").innerHTML = "You are " + Person.token;                            
-            }else{
-                document.getElementById("who").innerHTML = "";
-            }
-            Board = response.Board;
-            buildBoard();
-        }
-    }
-    console.log("sendObj" + sendObj);
-    xhttp.open("POST", '/', true);
-    xhttp.send(sendObj);
 }
 
 function reset(){
-    document.getElementById("warning").innerHTML = "resetting...";
-    sendObj = {text : "reset",
-                Person : Person};
+    sendObj.text = "reset";
+    sendObj.Player = null;
+    sendObj.Move = [0,0];
     POSTToServer(JSON.stringify(sendObj));
-    document.getElementById("warning").innerHTML = "";
+    document.getElementById("info").innerHTML = "Reset";
     document.getElementById("who").innerHTML = "";
 }
 
 function handleUpdate(){
     console.log("polling update...");
-    sendObj = {text : "update",
-                Person : Person};
+    sendObj.text = "update";
     POSTToServer(JSON.stringify(sendObj));
-    
+}
+
+function POSTToServer(JSONsendObj){
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status == 200){
+            responseObj = JSON.parse(this.responseText);
+            sendObj.Player = responseObj.Player;
+            buildBoard(responseObj.Board, responseObj.movingString);
+            document.getElementById("info").innerHTML = responseObj.info;
+            if(responseObj.Player){
+                document.getElementById("who").innerHTML = "You are " + responseObj.Player;                            
+            }else{
+                document.getElementById("who").innerHTML = "";                            
+            }   
+        }
+    }
+    //console.log("sendObj" + sendObj);
+    xhttp.open("POST", '/', true);
+    xhttp.send(JSONsendObj);
 }
 
 document.addEventListener('DOMContentLoaded', function(){
@@ -156,6 +136,6 @@ document.addEventListener('DOMContentLoaded', function(){
     //user clicks the canvas
     canvas.addEventListener('click', handleCanvasClick);
 
-    pollingTimer = setInterval(handleUpdate, 1000); //one second
+    pollingTimer = setInterval(handleUpdate, 400); //one second
 
 });
